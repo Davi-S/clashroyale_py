63db44771c7f02e72e2abb909c31c4e6eec0ae4d
Davi S <davialvessampaio00@gmail.com>
2024-03-01 21:38:36 -0300

    Renamed "args" parameter

--- clashroyale_py/models.py
+++ clashroyale_py/models.py
@@ -4,13 +4,12 @@ from box import Box, BoxList
 
 
 class ClashRoyaleModel:
-    # TODO: Use generic types for return values. Specially for the "data" and "raw_data"
-    def __init__(self, *args, is_from_cache: bool, timestamp: datetime, **kwargs) -> None:
-        if isinstance(args[0], list):
-            self._data = BoxList(*args, **kwargs)
+    def __init__(self, data: dict | list, is_from_cache: bool, timestamp: datetime, **kwargs) -> None:
+        if isinstance(data, list):
+            self._data = BoxList(data, **kwargs)
         else:
-            self._data = Box(*args, **kwargs)
-        self._raw_data = args[0]
+            self._data = Box(data, **kwargs)
+        self._raw_data = data
         self._is_from_cache = is_from_cache
         self._timestamp = timestamp
 
